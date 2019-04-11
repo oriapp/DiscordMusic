cabed07d3ffa11b16d1d54b62e7a34a6fac5c053
R C N <37003700+oriapp@users.noreply.github.com>
2019-04-12 01:35:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-exports.TOKEN = "NTY1NjcyOTI5MTQ5NDUyMjkw.XK52Pg.4sZMNT2TitbqS-W2dJMXCthBs7M";
+exports.TOKEN = "YOUR-TOKEN-HERE";
 
 
 exports.PREFIX = '!';
 
 
 
-exports.GOOGLE_API_KEY = 'AIzaSyBcwu1SywcDUTfyRTFVVBtOUmllvVu5lzY';
+exports.GOOGLE_API_KEY = 'GOOGLE-API-KEY-HERE';
